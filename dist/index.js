877a56cc2ddb1067762565b5e9856eec7d77180d
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-02-16 11:56:33 +0500

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let age = 20;
+console.log(age);
+if (age > 10) {
+    age + 10;
+    console.log(age);
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let age: number = 20;
+console.log(age);
+if (age > 10) {
+  age + 10;
+  console.log(age);
+}
