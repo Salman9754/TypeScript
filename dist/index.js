0ba2884c335bc35e9e8375241860a282123c3c38
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-02-16 16:34:25 +0500

    Classes And Inheritance

--- dist/index.js
+++ dist/index.js
@@ -10,4 +10,73 @@ const newUser = {
     isActive: true,
 };
 createUser(newUser);
+function processInput(input) {
+    if (typeof input === "number") {
+        return `Number is: ${input.toString()}`;
+    }
+    else {
+        return `String is: ${input}`;
+    }
+}
+let result = processInput(42);
+console.log(typeof result);
+processInput("Hello");
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    getInfo() {
+        return `make: ${this.make}, model: ${this.model}, year: ${this.year}`;
+    }
+}
+const myVehicle = new Vehicle("japan", "M5 CS", 2024);
+console.log(myVehicle.getInfo());
+class car extends Vehicle {
+    constructor(make, model, year, doors) {
+        super(make, model, year);
+        this.doors = doors;
+    }
+    getInfo() {
+        return `make: ${this.make}, model: ${this.model}, year: ${this.year}, doors:${this.doors}`;
+    }
+}
+const myCar = new car("BMW", "M4", 2025, 4);
+console.log(myCar.getInfo());
+class bike extends Vehicle {
+    constructor(make, model, year, hasSidecar) {
+        super(make, model, year);
+        this.hasSidecar = hasSidecar;
+    }
+    getInfo() {
+        return `make: ${this.make}, model: ${this.model}, year: ${this.year}, hasSieCar:${this.hasSidecar}`;
+    }
+}
+const myBike = new bike("Honda", "CG 125", 2021, true);
+console.log(myBike.getInfo());
+class BankAccount {
+    constructor(balance, account) {
+        (this.balance = balance), (this.accountNumber = account);
+    }
+    get checkBalance() {
+        return this.balance;
+    }
+    get checkAccountNo() {
+        return this.accountNumber;
+    }
+    addMoney(amount) {
+        return this.balance += amount;
+    }
+    withDraw(amount) {
+        return this.balance -= amount;
+    }
+}
+const myAcc = new BankAccount(10000, "salman12345");
+console.log(myAcc.checkBalance);
+console.log(myAcc.checkAccountNo);
+myAcc.withDraw(2000);
+console.log(myAcc.checkBalance);
+myAcc.addMoney(4000);
+console.log(myAcc.checkBalance);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// ***************** 1 *****************
 interface User {
   id: number;
   name: string;
@@ -17,3 +18,93 @@ const newUser: User = {
   isActive: true,
 };
 createUser(newUser);
+
+// ***************** 2 *****************
+type input = string | number;
+function processInput(input: input): string {
+  if (typeof input === "number") {
+    return `Number is: ${input.toString()}`;
+  } else {
+    return `String is: ${input}`;
+  }
+}
+let result = processInput(42);
+console.log(typeof result);
+
+processInput("Hello");
+
+// ***************** 3 *****************
+class Vehicle {
+  make: string;
+  model: string;
+  year: number;
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  getInfo(): string {
+    return `make: ${this.make}, model: ${this.model}, year: ${this.year}`;
+  }
+}
+const myVehicle = new Vehicle("japan", "M5 CS", 2024);
+console.log(myVehicle.getInfo());
+
+class car extends Vehicle {
+  doors: number;
+  constructor(make: string, model: string, year: number, doors: number) {
+    super(make, model, year);
+    this.doors = doors;
+  }
+  getInfo(): string {
+    return `make: ${this.make}, model: ${this.model}, year: ${this.year}, doors:${this.doors}`;
+  }
+}
+const myCar = new car("BMW", "M4", 2025, 4);
+console.log(myCar.getInfo());
+
+class bike extends Vehicle {
+  hasSidecar: boolean;
+  constructor(make: string, model: string, year: number, hasSidecar: boolean) {
+    super(make, model, year);
+    this.hasSidecar = hasSidecar;
+  }
+  getInfo(): string {
+    return `make: ${this.make}, model: ${this.model}, year: ${this.year}, hasSieCar:${this.hasSidecar}`;
+  }
+}
+
+const myBike = new bike("Honda", "CG 125", 2021, true);
+console.log(myBike.getInfo());
+
+// ***************** 4 *****************
+class BankAccount {
+  private balance: number;
+  private readonly accountNumber: string;
+  constructor(balance: number, account: string) {
+    (this.balance = balance), (this.accountNumber = account);
+  }
+  get checkBalance() {
+    return this.balance;
+  }
+  get checkAccountNo() {
+    return this.accountNumber;
+  }
+   addMoney(amount: number) {
+    return this.balance += amount;
+  }
+   withDraw(amount: number) {
+    return this.balance -= amount;
+  }
+}
+const myAcc = new BankAccount(10000, "salman12345");
+console.log(myAcc.checkBalance);
+console.log(myAcc.checkAccountNo);
+myAcc.withDraw(2000);
+console.log(myAcc.checkBalance);
+myAcc.addMoney(4000)
+console.log(myAcc.checkBalance);
+// console.log(myAcc.accountNumber);
+
+// ***************** 5 *****************
+
